6cc3359f366db6c00bf75837e00d6d03208ed194
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-11-08 13:08:29 -0300

    feat: create search form & display results

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,26 @@
 class FlightsController < ApplicationController
   def index
-    @flights = Flight.all
+    @airports = Airport.pluck(:code, :id)
+    @start_year, @end_year = Flight.year_range
+
+    @flights = Flight.filter_by_airports(params)
+
+    if params["[date(1i)]"].present?
+      @flights = @flights.select do |flight|
+        flight.start.year == params["[date(1i)]"].to_i
+      end
+    end
+
+    if params["[date(2i)]"].present?
+      @flights = @flights.select do |flight|
+        flight.start.month == params["[date(2i)]"].to_i
+      end
+    end
+
+    if params["[date(3i)]"].present?
+      @flights = @flights.select do |flight|
+        flight.start.day == params["[date(3i)]"].to_i
+      end
+    end
   end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,4 +1,15 @@
 class Flight < ApplicationRecord
   belongs_to :departure_airport, class_name: "Airport"
   belongs_to :arrival_airport, class_name: "Airport"
+
+  def self.year_range
+    [ minimum(:start).year, maximum(:start).year ]
+  end
+
+  def self.filter_by_airports(params)
+    order(:start).where(
+      departure_airport_id: params[:departure_airport_id],
+      arrival_airport_id: params[:arrival_airport_id]
+    )
+  end
 end
