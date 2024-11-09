00e363362c564c4ee990ed0774f8f7f97f36abe9
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-11-09 16:29:21 -0300

    chore(db): migrate the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_05_133219) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_09_185851) do
   create_table "airports", force: :cascade do |t|
     t.string "code"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.integer "flight_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["flight_id"], name: "index_bookings_on_flight_id"
+  end
+
+  create_table "bookings_passengers", id: false, force: :cascade do |t|
+    t.integer "booking_id", null: false
+    t.integer "passenger_id", null: false
+    t.index ["booking_id", "passenger_id"], name: "index_bookings_passengers_on_booking_id_and_passenger_id"
+    t.index ["passenger_id", "booking_id"], name: "index_bookings_passengers_on_passenger_id_and_booking_id"
   end
 
   create_table "flights", force: :cascade do |t|
@@ -28,6 +42,14 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_05_133219) do
     t.index ["departure_airport_id"], name: "index_flights_on_departure_airport_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "bookings", "flights"
   add_foreign_key "flights", "airports", column: "arrival_airport_id"
   add_foreign_key "flights", "airports", column: "departure_airport_id"
 end
