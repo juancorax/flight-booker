4f959508cbbc8adf631b196e2ffeaf13e2f7111b
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-11-13 17:57:31 -0300

    feat: add validations for passenger model

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,4 +1,7 @@
 class Passenger < ApplicationRecord
   has_and_belongs_to_many :bookings, dependent: :destroy
   has_many :flights, through: :bookings, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
