aea34ca7db6c1d9ccab5420fd69605a6d9a9f721
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-11-13 17:46:32 -0300

    feat: allow the creation of bookings & passengers from a single form

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,33 @@
 class BookingsController < ApplicationController
+  def show
+    @booking = Booking.find(params[:id])
+    @flight = @booking.flight
+  end
+
+  def new
+    @flight = Flight.find(params[:flight_id])
+
+    @booking = Booking.new
+
+    number_of_passengers = params[:number_of_passengers].to_i
+
+    number_of_passengers.times do
+      @booking.passengers.build
+    end
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: "Booking created"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+    def booking_params
+      params.require(:booking).permit(:flight_id, :number_of_passengers, passengers_attributes: [ :name, :email ])
+    end
 end
