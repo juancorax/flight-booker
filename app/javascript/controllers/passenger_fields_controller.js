43a6bb3f4bfff3b7cd0b7f65a1a1fce441612806
Juan Manuel Corigliano Giuliani <126042007+juancorax@users.noreply.github.com>
2024-11-28 17:41:15 -0300

    feat: allow adding and removing passengers

--- app/javascript/controllers/passenger_fields_controller.js
+++ app/javascript/controllers/passenger_fields_controller.js
@@ -1,7 +1,65 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="passenger-fields"
 export default class extends Controller {
+  static targets = ["list", "addButton", "removeButton", "template"];
+
+  get passengerCount() {
+    return this.listTarget.querySelectorAll("li").length;
+  }
+
+  get isAtMinPassengers() {
+    return this.passengerCount <= 1;
+  }
+
+  get isAtMaxPassengers() {
+    return this.passengerCount >= 4;
+  }
+
   connect() {
+    this.displayButtons();
+  }
+
+  displayButtons() {
+    this.removeButtonTargets.forEach((button) => {
+      button.hidden = this.isAtMinPassengers;
+    });
+
+    this.addButtonTarget.hidden = this.isAtMaxPassengers;
+  }
+
+  addPassenger(event) {
+    event.preventDefault();
+
+    if (this.isAtMaxPassengers) {
+      return;
+    }
+
+    const templateContent = this.templateTarget.content.cloneNode(true);
+
+    const newIndex = this.passengerCount;
+
+    templateContent.querySelectorAll("[name]").forEach((element) => {
+      element.name = element.name.replace(/\[\d+\]/, `[${newIndex}]`);
+    });
+    templateContent.querySelectorAll("[id]").forEach((element) => {
+      element.id = element.id.replace(/_\d+_/, `_${newIndex}_`);
+    });
+
+    this.listTarget.appendChild(templateContent);
+
+    this.displayButtons();
+  }
+
+  removePassenger(event) {
+    event.preventDefault();
+
+    if (this.isAtMinPassengers) {
+      return;
+    }
+
+    event.target.closest("li").remove();
+
+    this.displayButtons();
   }
 }
